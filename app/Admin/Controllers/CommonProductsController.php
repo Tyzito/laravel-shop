ba675ee6f85a6b0be5eac3ad21baa6132dfd79fe
vagrant <vagrant@homestead>
2021-03-17 14:13:41 +0000

    管理后台商品控制器封装

--- app/Admin/Controllers/CommonProductsController.php
+++ app/Admin/Controllers/CommonProductsController.php
@@ -15,19 +15,18 @@ abstract class CommonProductsController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new Product());
-
-        // 筛选出当前类型的商品，默认 ID 倒序排序
+        // 筛选出当前类型的商品，默认ID倒序排序
         $grid->model()->where('type', $this->getProductType())->orderBy('id', 'desc');
-
-        // 调用自定义方法
+        // 调用自定义的方法
         $this->customGrid($grid);
 
-        $grid->actions(function ($actions) {
+        $grid->actions(function ($actions){
             $actions->disableView();
             $actions->disableDelete();
         });
-        $grid->tools(function ($tools) {
-            $tools->batch(function ($batch) {
+
+        $grid->tools(function ($tools){
+            $tools->batch(function ($batch){
                 $batch->disableDelete();
             });
         });
@@ -41,35 +40,46 @@ abstract class CommonProductsController extends AdminController
     protected function form()
     {
         $form = new Form(new Product());
-        // 在表单页面中添加一个名为 type 的隐藏字段，值为当前商品类型
+
         $form->hidden('type')->value($this->getProductType());
-        $form->text('title', '商品名称')->rules('required');
-        $form->select('category_id', '类目')->options(function ($id) {
+
+        // 创建一个输入框
+        $form->text('title','商品名称')->rules('required');
+
+        // 添加一个类目字段，与之前类目管理类似，使用 Ajax 的方式搜索添加
+        $form->select('category_id', '类目')->options(function ($id){
             $category = Category::find($id);
-            if ($category) {
+            if($category){
                 return [$category->id => $category->full_name];
             }
         })->ajax('/admin/api/categories?is_directory=0');
-        $form->image('image', '封面图片')->rules('required|image');
-        $form->quill('description', '商品描述')->rules('required');
-        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
+
+        // 创建一个选择图片的框
+        $form->image('image','选择图片')->rules('required|image');
+        // 创建一个富文本编辑器
+        $form->quill('description','商品描述')->rules('required');
+        // 创建一组单选框
+        $form->radio('on_sale','上架')->options(['1' => '是','0' => '否'])->default('0');
 
         // 调用自定义方法
         $this->customForm($form);
 
-        $form->hasMany('skus', '商品 SKU', function (Form\NestedForm $form) {
-            $form->text('title', 'SKU 名称')->rules('required');
-            $form->text('description', 'SKU 描述')->rules('required');
-            $form->text('price', '单价')->rules('required|numeric|min:0.01');
-            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
+        // 直接添加一组关联模型
+        $form->hasMany('skus','SKU 列表',function (Form\NestedForm $form){
+            $form->text('title','SKU 名称')->rules('required');
+            $form->text('description','SKU 描述')->rules('required');
+            $form->text('price','单价')->rules('required|numeric|min:0.01');
+            $form->text('stock','剩余库存')->rules('required|integer|min:0');
         });
-        $form->saving(function (Form $form) {
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
+
+        // 定义事件回调，当模型即将保存时会触发这个回调
+        $form->saving(function (Form $form){
+            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME,0)->min('price') ?: 0;
         });
 
         return $form;
     }
 
     // 定义一个抽象方法，各个类型的控制器将实现本方法来定义表单应该有哪些额外的字段
-    abstract protected function customForm(Form $form);
+   abstract protected function customForm(Form $form);
 }
--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
@@ -15,34 +16,29 @@ class ProductsController extends CommonProductsController
      */
     protected $title = '商品';
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
+    public function getProductType()
+    {
+        return Product::TYPE_NORMAL;
+    }
+
     protected function customGrid(Grid $grid)
     {
         $grid->model()->with(['category']);
         $grid->id('ID')->sortable();
         $grid->title('商品名称');
+        // Laravel-Admin 支持用符号 . 来展示关联关系的字段
         $grid->column('category.name', '类目');
-        $grid->on_sale('已上架')->display(function ($value) {
+        $grid->on_sale('已上架')->display(function ($value){
             return $value ? '是' : '否';
         });
         $grid->price('价格');
         $grid->rating('评分');
-        $grid->sold_count('销量');
+        $grid->sole_count('销量');
         $grid->review_count('评论数');
     }
 
     protected function customForm(Form $form)
     {
-        // 普通商品没有额外的字段，因此这里不需要写任何代码
-    }
-
-    public function getProductType()
-    {
-        // 普通商品
-        return Product::TYPE_NORMAL;
+        // 普通商品没有额外的字段，这里不需要写任何代码
     }
 }
